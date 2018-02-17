e3a6dd328b48b530c41c4d5c1dd8631171ffd51a
Natalie <miteiru@gmail.com>
2018-02-17 21:02:18 +0900

    Added all types from module 2

--- script.js
+++ script.js
@@ -1,16 +1,105 @@
-var Greeter = /** @class */ (function () {
-    function Greeter(message) {
-        this.greeting = message;
-    }
-    Greeter.prototype.greet = function () {
-        return "Hello, " + this.greeting;
-    };
-    return Greeter;
-}());
-var greeter = new Greeter("world");
-var button = document.createElement('button');
-button.textContent = "Say Hello";
-button.onclick = function () {
-    alert(greeter.greet());
+"use strict";
+//string
+var myName = 'Natalie';
+//number
+var myAge = 25;
+//boolean
+var hasHobbies = true;
+//assign Types
+var myRealAge;
+myRealAge = 27;
+// myRealAge = "27";
+//array
+var hobbies = ["Cooking", "Piano"];
+hobbies = [100];
+console.log(typeof hobbies);
+//tuples(array with mixed types and limited number)
+var address = ["Superstreet", 90];
+//enum (make numbers more expressive)
+var Color;
+(function (Color) {
+    Color[Color["Gray"] = 0] = "Gray";
+    Color[Color["Green"] = 100] = "Green";
+    Color[Color["Blue"] = 101] = "Blue";
+})(Color || (Color = {}));
+var myColor = Color.Blue; //displays 101
+console.log(myColor);
+//any
+var car = "BMW";
+console.log(car);
+car = { brand: "BMW", series: 3 };
+console.log(car);
+//functions
+function returnMyName() {
+    return myName;
+}
+console.log(myName);
+//void (no return statement)
+function sayHello() {
+    console.log("Hello!");
+}
+//argument types
+function multiply(value1, value2) {
+    return value1 * value2;
+}
+// console.log(multiply(2,"Max"));
+console.log(multiply(2, 10));
+//function types (assigning by ES6 arrow syntaxis)
+var myMultiply; //defines arguments and return types
+myMultiply = multiply;
+console.log(multiply(5, 2));
+//objects
+var userData = {
+    name: "Max",
+    age: 27
 };
-document.body.appendChild(button);
+//complex objects
+//data is an array of numbers
+//output is a boolean function with one boolean type argument which return array of numbers
+var complex = {
+    data: [100, 99, 98],
+    ouput: function (all) {
+        return this.data;
+    }
+};
+console.log(complex);
+var complex2 = {
+    data: [100, 99, 98],
+    ouput: function (all) {
+        return this.data;
+    }
+};
+//multiple types with union types (assigns with OR(single pipe line))
+var myRealRealAge = 27;
+myRealRealAge = "27";
+//check types
+var finalValue = 20;
+if (typeof finalValue == "number") {
+    console.log("Final value is a number");
+}
+//never type (TypeScript 2.0)
+//not to get confused with void!
+//not returns nothing, but never returns anything
+function neverReturns() {
+    throw new Error("an Error!");
+}
+//nullable types(TypeScript 2.0)
+var canBeNull = 12; // automatically assigns number type, but null can be assigned as type
+// canBeNull = null;
+var canAlsoBeNull; //any type
+canAlsoBeNull = null;
+var canThisBeAny = null;
+canThisBeAny = 12;
+var bankAccount = {
+    money: 2000,
+    deposit: function (value) {
+        this.money += value;
+    }
+};
+var myself = {
+    name: "Natalie",
+    bankAccount: bankAccount,
+    hobbies: ["Piano", "Games"]
+};
+myself.bankAccount.deposit(3000);
+console.log(myself);
--- script.ts
+++ script.ts
@@ -1,19 +1,147 @@
-class Greeter {
-  greeting: string;
-  constructor(message: string) {
-    this.greeting = message;
+//string
+let myName = 'Natalie';
+//number
+let myAge = 25;
+//boolean
+let hasHobbies: boolean = true;
+
+//assign Types
+let myRealAge: number;
+myRealAge = 27;
+// myRealAge = "27";
+
+//array
+let hobbies: any[] = ["Cooking", "Piano"];
+hobbies = [100];
+console.log(typeof hobbies)
+
+//tuples(array with mixed types and limited number)
+let address: [string, number] = ["Superstreet", 90];
+
+//enum (make numbers more expressive)
+enum Color {
+  Gray,
+  Green = 100,
+  Blue
+}
+
+let myColor: Color = Color.Blue;//displays 101
+console.log(myColor);
+
+//any
+let car: any = "BMW";
+console.log(car);
+car = { brand: "BMW", series: 3 };
+console.log(car);
+
+//functions
+function returnMyName(): string {
+  return myName;
+}
+console.log(myName);
+
+//void (no return statement)
+function sayHello(): void {
+  console.log("Hello!")
+}
+
+//argument types
+function multiply(value1: number, value2: number): number {
+  return value1 * value2;
+}
+// console.log(multiply(2,"Max"));
+console.log(multiply(2, 10));
+
+//function types (assigning by ES6 arrow syntaxis)
+let myMultiply: (val1: number, val2: number) => number;//defines arguments and return types
+myMultiply = multiply;
+console.log(multiply(5, 2));
+
+//objects
+let userData: { name: string, age: number } = {
+  name: "Max",
+  age: 27
+};
+
+//complex objects
+//data is an array of numbers
+//output is a boolean function with one boolean type argument which return array of numbers
+let complex: { data: number[], ouput: (all: boolean) => number[] } = {
+  data:[100, 99, 98],
+  ouput: function(all: boolean): number[] {
+    return this.data;
   }
-  greet() {
-    return "Hello, " + this.greeting;
+}
+console.log(complex);
+
+//alias for storing types
+type Complex = { data: number[], ouput: (all: boolean) => number[] };
+
+let complex2: Complex = {
+  data:[100, 99, 98],
+  ouput: function(all: boolean): number[] {
+    return this.data;
   }
 }
 
-let greeter = new Greeter("world");
+//multiple types with union types (assigns with OR(single pipe line))
+let myRealRealAge: number | string = 27;
+myRealRealAge = "27";
 
-let button = document.createElement('button');
-button.textContent = "Say Hello";
-button.onclick = function() {
-  alert(greeter.greet());
+//check types
+let finalValue = 20;
+if (typeof finalValue == "number") {
+  console.log("Final value is a number");
 }
 
-document.body.appendChild(button);
+//never type (TypeScript 2.0)
+//not to get confused with void!
+//not returns nothing, but never returns anything
+function neverReturns(): never{
+  throw new Error("an Error!")
+}
+
+//nullable types(TypeScript 2.0)
+let canBeNull = 12; // automatically assigns number type, but null can be assigned as type
+// canBeNull = null;
+let canAlsoBeNull; //any type
+canAlsoBeNull = null;
+let canThisBeAny = null;
+canThisBeAny = 12;
+
+//type excercise
+//original
+// let bankAccount = {
+//   money: 2000,
+//   deposit(value) {
+//     this.money += value;
+//   }
+// };
+//
+// let myself = {
+//   name: "Natalie",
+//   bankAccount: bankAccount,
+//   hobbies: ["Piano", "Games"]
+// };
+//
+// myself.bankAccount.deposit(3000);
+// console.log(myself);
+
+//with types
+type BankAccount = {money: number, deposit: (value: number) => void};
+
+let bankAccount: BankAccount = {
+  money: 2000,
+  deposit(value:number): void {
+    this.money += value;
+  }
+};
+
+let myself: {name: string, bankAccount: BankAccount, hobbies: string[]} = {
+  name: "Natalie",
+  bankAccount: bankAccount,
+  hobbies: ["Piano", "Games"]
+};
+
+myself.bankAccount.deposit(3000);
+console.log(myself);
