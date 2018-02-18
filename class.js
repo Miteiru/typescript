c2d116743e7bf8f78534ce2bfabd3e6b159bf5b1
Natalie <miteiru@gmail.com>
2018-02-18 14:35:10 +0900

    Competed class module Added Compile on save feature in config

--- class.js
+++ class.js
@@ -1,4 +1,14 @@
 "use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var Person = /** @class */ (function () {
     //creating new property in constructor
     function Person(name, userName) {
@@ -15,8 +25,172 @@ var Person = /** @class */ (function () {
     };
     return Person;
 }());
-var person = new Person("Natalie", "nata");
-console.log(person.name);
-console.log(person.userName);
-person.printAge();
-person.setType("Nice girl");
+var user = new Person("Natalie", "nata");
+console.log(user.name);
+console.log(user.userName);
+user.printAge();
+user.setType("Nice girl");
+//inheritance
+var Natalie = /** @class */ (function (_super) {
+    __extends(Natalie, _super);
+    function Natalie(userName) {
+        var _this = _super.call(this, "Natalie", userName) || this;
+        _this.age = 17;
+        return _this;
+    }
+    return Natalie;
+}(Person));
+var natalie = new Natalie("nata");
+console.log(natalie);
+//Getters and Setters
+var Plant = /** @class */ (function () {
+    function Plant() {
+        this._species = "Default";
+    }
+    Object.defineProperty(Plant.prototype, "species", {
+        get: function () {
+            return this._species;
+        },
+        set: function (value) {
+            if (value.length > 3) {
+                this._species = value;
+            }
+            else {
+                this._species = "Default";
+            }
+        },
+        enumerable: true,
+        configurable: true
+    });
+    return Plant;
+}());
+var plant = new Plant();
+console.log(plant.species);
+plant.species = "ABCD";
+console.log(plant.species);
+//Ststic Properties an methods
+var Helpers = /** @class */ (function () {
+    function Helpers() {
+    }
+    Helpers.calcCircumference = function (diameter) {
+        return this.PI * diameter;
+    };
+    Helpers.PI = 3.14;
+    return Helpers;
+}());
+console.log(2 * Helpers.PI); //no need to create object for static
+console.log(Helpers.calcCircumference(8));
+//abstract classes
+var Project = /** @class */ (function () {
+    function Project() {
+        this.budget = 1000;
+    }
+    Project.prototype.calcBudget = function () {
+        return this.budget * 2;
+    };
+    return Project;
+}());
+var ITProject = /** @class */ (function (_super) {
+    __extends(ITProject, _super);
+    function ITProject() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    ITProject.prototype.changeName = function (name) {
+        this.projectName = name;
+    };
+    return ITProject;
+}(Project));
+var newProject = new ITProject();
+console.log(newProject);
+newProject.changeName("Super IT Project");
+console.log(newProject);
+//private constructors
+var OnlyOne = /** @class */ (function () {
+    function OnlyOne(name) {
+        this.name = name;
+    }
+    OnlyOne.getInstance = function () {
+        if (!OnlyOne.instance) {
+            OnlyOne.instance = new OnlyOne("The Only One");
+        }
+        return OnlyOne.instance;
+    };
+    return OnlyOne;
+}());
+//let wrong = new OnlyOne("The Only One");//Error
+var right = OnlyOne.getInstance();
+console.log(right.name);
+//right.name = "Something Else";can't assign becaouse readonly
+console.log(right.name);
+//excercise
+// Exercise 1 - How was your TypeScript Class?
+var Car = /** @class */ (function () {
+    function Car(name) {
+        this.acceleration = 0;
+        this.name = name;
+    }
+    Car.prototype.honk = function () {
+        console.log("Toooooooooot!");
+    };
+    Car.prototype.accelerate = function (speed) {
+        this.acceleration += speed;
+    };
+    return Car;
+}());
+var car1 = new Car("BMW");
+car1.honk();
+console.log(car1.acceleration);
+car1.accelerate(10);
+console.log(car1.acceleration);
+// Exercise 2 - Two objects, based on each other ...
+var BaseObject = /** @class */ (function () {
+    function BaseObject() {
+        this.width = 0;
+        this.length = 0;
+    }
+    return BaseObject;
+}());
+;
+var Rectangle = /** @class */ (function (_super) {
+    __extends(Rectangle, _super);
+    function Rectangle() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    Rectangle.prototype.calcSize = function () {
+        return this.width * this.length;
+    };
+    return Rectangle;
+}(BaseObject));
+;
+var rectangle = new Rectangle();
+rectangle.width = 5;
+rectangle.length = 5;
+console.log(rectangle.calcSize());
+// Exercise 3 - Make sure to compile to ES5 (set the target in tsconfig.json)
+var Person = /** @class */ (function () {
+    function Person() {
+    }
+    Object.defineProperty(Person.prototype, "firstName", {
+        get: function () {
+            return this._firstName;
+        },
+        set: function (value) {
+            if (value.length > 3) {
+                this._firstName = value;
+            }
+            else {
+                this._firstName = "";
+            }
+        },
+        enumerable: true,
+        configurable: true
+    });
+    return Person;
+}());
+;
+var user = new Person();
+console.log(user.firstName); //empty
+user.firstName = "Ma"; //empty
+console.log(user.firstName);
+user.firstName = "Maximilian";
+console.log(user.firstName);
--- class.ts
+++ class.ts
@@ -2,8 +2,8 @@ class Person {
   public name: string;
   private type: string;
   protected age: number = 25;
-                            //creating new property in constructor
-  constructor(name: string, public userName: string){
+  //creating new property in constructor
+  constructor(name: string, public userName: string) {
     this.name = name;
   }
 
@@ -17,8 +17,158 @@ class Person {
   }
 }
 
-const person =new Person("Natalie","nata");
-console.log(person.name);
-console.log(person.userName);
-person.printAge();
-person.setType("Nice girl");
+const user = new Person("Natalie", "nata");
+console.log(user.name);
+console.log(user.userName);
+user.printAge();
+user.setType("Nice girl");
+
+//inheritance
+class Natalie extends Person {
+  constructor(userName: string) {
+    super("Natalie", userName);
+    this.age = 17;
+  }
+}
+
+const natalie = new Natalie("nata");
+console.log(natalie);
+
+//Getters and Setters
+class Plant {
+  private _species: string = "Default";
+
+  get species() {
+    return this._species;
+  }
+
+  set species(value: string) {
+    if (value.length > 3) {
+      this._species = value;
+    } else {
+      this._species = "Default";
+    }
+  }
+}
+
+let plant = new Plant();
+console.log(plant.species);
+plant.species = "ABCD";
+console.log(plant.species);
+
+//Ststic Properties an methods
+class Helpers {
+  static PI: number = 3.14;
+  static calcCircumference(diameter: number): number {
+    return this.PI * diameter;
+  }
+}
+console.log(2 * Helpers.PI);//no need to create object for static
+console.log(Helpers.calcCircumference(8));
+
+//abstract classes
+abstract class Project {
+  projectName: string;
+  budget: number = 1000;
+
+  abstract changeName(name: string): void;//abstract methods only define form with no logic
+
+  calcBudget() {
+    return this.budget * 2;
+  }
+}
+
+class ITProject extends Project {
+  changeName(name: string): void {//must override abstract method!
+    this.projectName = name;
+  }
+}
+
+let newProject = new ITProject();
+console.log(newProject);
+newProject.changeName("Super IT Project");
+console.log(newProject);
+
+//private constructors
+class OnlyOne {
+  private static instance: OnlyOne;
+  private constructor(public readonly name: string) { }
+  static getInstance() {
+    if (!OnlyOne.instance) {
+      OnlyOne.instance = new OnlyOne("The Only One");
+    }
+    return OnlyOne.instance;
+  }
+}
+
+//let wrong = new OnlyOne("The Only One");//Error
+let right = OnlyOne.getInstance();
+console.log(right.name);
+//right.name = "Something Else";can't assign becaouse readonly
+console.log(right.name);
+
+//excercise
+// Exercise 1 - How was your TypeScript Class?
+class Car {
+  name: string;
+  acceleration: number = 0;
+
+  constructor(name: string) {
+    this.name = name;
+  }
+
+  honk() {
+    console.log("Toooooooooot!");
+  }
+
+  accelerate(speed: number) {
+     this.acceleration += speed;
+  }
+}
+
+const car1 = new Car("BMW");
+car1.honk();
+console.log(car1.acceleration);
+car1.accelerate(10);
+console.log(car1.acceleration);
+
+// Exercise 2 - Two objects, based on each other ...
+class BaseObject {
+  width: number = 0;
+  length: number = 0;
+};
+
+class Rectangle extends BaseObject {
+   calcSize() {
+    return this.width * this.length;
+  }
+};
+
+const rectangle = new Rectangle();
+rectangle.width = 5;
+rectangle.length = 5;
+console.log(rectangle.calcSize());
+
+// Exercise 3 - Make sure to compile to ES5 (set the target in tsconfig.json)
+class Person {
+  private _firstName: string;
+
+  get firstName() {
+    return this._firstName;
+  }
+
+  set firstName(value: string) {
+    if (value.length > 3) {
+      this._firstName = value;
+    }
+    else {
+      this._firstName = "";
+    }
+  }
+};
+const user = new Person();
+console.log(user.firstName);//empty
+user.firstName = "Ma";//empty
+console.log(user.firstName);
+user.firstName = "Maximilian";
+console.log(user.firstName);
